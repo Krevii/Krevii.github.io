520624de2a7a907fdf5f9e14cdc560ac9d3cdd80
Krevii <krevetkatop360@gmail.com>
2022-09-11 23:46:23 +0600

    Fixx

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let card = document.querySelector("#IS-lab1");
 
 card.addEventListener("click", ()=>{
-    location.href = "Krevii.github.io/InformationSecurity/Lab1/index.html";
+    location.href = "InformationSecurity/Lab1/index.html";
 })
