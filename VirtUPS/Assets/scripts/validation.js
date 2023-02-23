9f5566345e76c43bca039f7928fec059215b616e
Krevii <98155172+Krevii@users.noreply.github.com>
2023-02-24 03:37:35 +0600

    BunFix

--- VirtUPS/Assets/scripts/validation.js
+++ VirtUPS/Assets/scripts/validation.js
@@ -1,15 +1,35 @@
-$("input[type = 'submit']").first().attr('disabled','');
+$("body").delegate("input[type = 'submit']","click", function (e) {
+    
+});
 
-$("body").delegate("#text-input","keyup", function (e) {
-    let tempalte = /\w{1,}@\w{1,}\.\w{1,}/i;
-    
-    $("input[type = 'submit']").first().attr('disabled','');
-    
-    if (!tempalte.test($("#text-input").val())) {
-        $(this).css("border","1px solid red");
+$("input[type = 'submit']").click(function (e) { 
+    let tempalteEmail = /\w{1,}@\w{1,}\.\w{1,}/i;
+    let tempalteInput = /.{1,}/i;
+    let tempaltePass = /.{8,}/i;
+
+    if (!tempalteInput.test($("input[name = 'first-name']").val())){
+        // $("input[name = 'first-name']").css("border-bottom","1px solid red");
+        alert("verify first name");
+        e.preventDefault()
+    }
+    else if (!tempalteInput.test($("input[name = 'last-name']").val())){
+        // $("input[name = 'last-name']").css("border-bottom","1px solid red");
+        alert("verify last name");
+        e.preventDefault()
+    }
+    else if (!tempalteEmail.test($("#text-input").val())) {
+        // $("#text-input").css("border-bottom","1px solid red");
+        alert("verify E-mail");
+        e.preventDefault()
+    }
+    else if (!tempaltePass.test($("input[name='password']").val())) {
+        // $("input[name='password']").css("border-bottom","1px solid red");
+        alert("verify password");
+        e.preventDefault()
     }
     else{
-        $(this).css("border","none");
-        $("input[type = 'submit']").removeAttr('disabled');
+        // $("input").css("border-bottom","1px solid var(--secondary-color)");
+        alert("Вы зарегистрированы");
+        e.preventDefault()
     }
 });
